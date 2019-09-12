c20cea1448c7984c8a2e545fed460d9d41fa525e
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-09-12 16:59:36 -0400

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskForm from './tasks/taskForm/TaskForm'
+
+function App() {
+    return (
+        <div>
+           <TaskForm /> 
+        </div>
+    )
+}
+
+export default App
