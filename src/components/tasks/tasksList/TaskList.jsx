2598cdfe742396120ca74e46b41c00ffb7a5c9a7
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-09-12 20:50:37 -0400

    Added an Add button to TaskList

--- src/components/tasks/tasksList/TaskList.jsx
+++ src/components/tasks/tasksList/TaskList.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Task from './Task';
+import {Link} from 'react-router-dom';
 
 function TaskList({tasks}) {
    
     return (
         <div>
            {tasks.map(task => <Task key={task.id} task={task} />)} 
+           <button> 
+            <Link to='/addtask'>
+                Add Task
+            </Link>
+           </button>
         </div>
     )
 }
